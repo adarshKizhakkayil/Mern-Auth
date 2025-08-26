9a2204bf2812d20b51ce9338c2ee4a8cd0a1e975
adarshkizhakkayil <adarshk09052001@gmail.com>
2025-08-26 19:13:32 +0530

    db

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,9 @@
-console.log('set up')
+import express from 'express';
+const app = express();
+
+app.listen(3000,()=>console.log('server runnin go on port 3000'))
+
+app.get('/',(req,res)=>{
+    console.log('Request received');
+    res.send('hi')
+})
